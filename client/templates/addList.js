4134035a0cf5423e1d2d5675fa94dee774531ce0
jbbpatel94 <jbbpatel94@gmail.com>
2017-07-29 00:06:36 +0530

    Redirecting Routes

--- client/templates/addList.js
+++ client/templates/addList.js
@@ -1,10 +1,12 @@
 Template.addList.events({
     'submit form': function(event){
-      event.preventDefault();
-      var listName = $('[name=listName]').val();
-      Lists.insert({
+        event.preventDefault();
+        var listName = $('[name=listName]').val();
+        Lists.insert({
           name: listName
-      });
-      $('[name=listName]').val('');
+        }, function(error, results){
+            Router.go('listPage', { _id: results });
+        });
+        $('[name=listName]').val('');
     }
 });
